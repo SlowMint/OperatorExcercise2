4b008698d92457eccfeeb3385dbb6f53b8b591cf
SlowMint <individualmotiv@gmail.com>
2022-07-04 14:08:56 -0500

    initial commit

---
+++ OperatorExercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OperatorExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is the radius of your circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+            var areaOfCircle = CalcArea(radius);
+
+            Console.WriteLine($"The area of your circle with a radius of {radius} is {areaOfCircle}.");
+        }
+        
+        public static double CalcArea (double radius )
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+}
